0f89b3a218a15affba8b62df58953dd0e76feba8
Sean Perry <sean.hyatt.perry@gmail.com>
2025-02-09 14:40:28 -0800

    feat: get old unmigrated changes (so the image works and I can test cli)

---
+++ scheduler/label_studio_scheduler.py
@@ -0,0 +1,26 @@
+import os 
+from kubernetes import client, config, utils
+import eventlet
+import socketio
+from os import path
+import yaml
+import pandas as pd
+import random
+import shutil
+import copy
+from label_studio_ml.api import init_app
+from maestro_model import MaestroModel, sio
+
+
+print(os.environ['TRAINER_IMAGE'])
+
+
+# This should handle the RESTful api requests from label studio
+# but still serve the socketio system for manging the backend cluster
+model_app = init_app(model_class=MaestroModel)
+app = socketio.WSGIApp(sio, model_app, static_files={
+    '/': {'content_type': 'text/html', 'filename': 'index.html'}
+})
+
+if __name__ == '__main__':
+    eventlet.wsgi.server(eventlet.listen(('', 3000)), app)
